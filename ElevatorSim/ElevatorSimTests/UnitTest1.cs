f8c7cc2db95b2c8c0bbff640c7b528cc336918cf
Jeremy Cruz <jere.cruz519@gmail.com>
2023-12-13 12:01:55 -0800

    added gitignore

---
+++ ElevatorSim/ElevatorSimTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using XUnit;
+
+namespace ElevatorSimTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
